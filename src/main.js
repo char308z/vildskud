e6a410c905197b430f12f31a61116637febd5c33
Charlotte Franciska Christiansen <char308@stud.kea.dk>
2022-10-10 14:53:11 +0200

    fælles header

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
 import "./style.css";
+
+const burgermenu = document.querySelector("input[type=checkbox]");
+
+burgermenu.addEventListener("change", showNav);
+
+function showNav() {
+  if (this.checked) {
+    document.querySelector("#burgerContent").classList.remove("hide");
+  } else {
+    document.querySelector("#burgerContent").classList.add("hide");
+  }
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#A00000",
+        secondary: "#DCA0A0",
+        sort: "#222222",
+        hvid: "#FDFDFD",
+      },
+      fontFamily: {
+        cond: ["HelveticaNeue-Condensed", "serif"],
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
