cd66f59bf6d53e32a7e847d473bd449cc5cb5e44
Charlotte Franciska Christiansen <char308@stud.kea.dk>
2022-10-13 19:47:37 +0200

    færdig forside

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,21 @@ module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      gridTemplateRows: {
+        // Simple 8 row grid
+        9: "repeat(9, minmax(100px, auto))",
+      },
+
+      gridRowStart: {
+        8: "8",
+        9: "9",
+      },
+
+      gridRowEnd: {
+        8: "8",
+        9: "9",
+        10: "10",
+      },
       colors: {
         primary: "#A00000",
         secondary: "#DCA0A0",
